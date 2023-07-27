433706a48f444e4b9e14c843049c164ebd1877cf
Jonathan-Lindbloom <jonathan@lindbloom.com>
2023-07-27 12:51:47 -0400

    added cupy?

--- src/runningstats/__init__.py
+++ src/runningstats/__init__.py
@@ -1,1 +1,10 @@
+# Check if CuPy installed or not
+CUPY_INSTALLED = False
+try:
+    import cupy
+    CUPY_INSTALLED = True
+except:
+    pass
+
+# Imports
 from .statstracker import StatsTracker
--- src/runningstats/statstracker.py
+++ src/runningstats/statstracker.py
@@ -1,20 +1,42 @@
 import numpy as np
 
+from . import CUPY_INSTALLED
+if CUPY_INSTALLED:
+    import cupy as cp
 
 
 class StatsTracker:
 
-    def __init__(self, array_shape):
+    def __init__(self, array_shape, device="cpu"):
         """
         array_shape: shape of the array whose stats we are tracking.
         compute: list of quantities we want to compute (by default all, but can omit some).
         """
+        
+        # Handle inputs
         self.array_shape = array_shape
+        if not ( isinstance(self.array_shape, tuple) or isinstance(self.array_shape, list) ):
+            if isinstance(array_shape, int):
+                self.array_shape = (self.array_shape,)
+            else:
+                raise ValueError("array_shape must be list-like or an integer!")
+
+        valid_devices = ["cpu", "gpu"]
+        assert device in valid_devices, f"invalid device, must be one of {valid_devices}"
+        self.device = device
+        if self.device == "cpu":
+            self.xp = np
+        elif self.device == "gpu" and CUPY_INSTALLED:
+            self.xp = cp
+        else:
+            raise ValueError("Need to install CuPy first if wanting to run on GPU.")
+
+        # Set up
         self.n_samples = 0 # number of samples that have been pushed so far
-        self.M1 = np.zeros(self.array_shape)
-        self.M2 = np.zeros(self.array_shape)
-        self.M3 = np.zeros(self.array_shape)
-        self.M4 = np.zeros(self.array_shape)
+        self.M1 = self.xp.zeros(self.array_shape)
+        self.M2 = self.xp.zeros(self.array_shape)
+        self.M3 = self.xp.zeros(self.array_shape)
+        self.M4 = self.xp.zeros(self.array_shape)
 
 
 
@@ -84,8 +106,8 @@ class StatsTracker:
         """Resets the tracker.
         """
         self.n_samples = 0
-        self.M1 = np.zeros(self.array_shape)
-        self.M2 = np.zeros(self.array_shape)
-        self.M3 = np.zeros(self.array_shape)
-        self.M4 = np.zeros(self.array_shape)
-        
+        self.M1 = self.xp.zeros(self.array_shape)
+        self.M2 = self.xp.zeros(self.array_shape)
+        self.M3 = self.xp.zeros(self.array_shape)
+        self.M4 = self.xp.zeros(self.array_shape)
+
